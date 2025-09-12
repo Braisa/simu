ecdc7293e97512a88bb23b7bae1a87a90e0e3cfc
Braisa <oterolemabrais@gmail.com>
2025-09-12 11:36:09 +0200

    Also reading bit size

--- utils/read_kinds.f90
+++ utils/read_kinds.f90
@@ -1,7 +1,14 @@
 program read_kinds
     use get_kinds
-    
+
     implicit none
+
+    integer (int) :: i
+    real (double) :: d
+
     print *, 'integer: ', int
     print *, 'double: ', double
+
+    print *, 'integer bits: ', STORAGE_SIZE(i, int)
+    print *, 'double bits: ', STORAGE_SIZE(d, double)
 end program read_kinds
