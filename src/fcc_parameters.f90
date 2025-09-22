f28adbb4a72e2b08df9bf1854394810ca0623ba1
Braisa <oterolemabrais@gmail.com>
2025-09-22 11:54:30 +0200

    LJ Potential subroutine Now properly declares output variables as dummy variables and returns them.

--- src/fcc_parameters.f90
+++ src/fcc_parameters.f90
@@ -4,7 +4,9 @@ module fcc_parameters
 
     implicit none
 
-    integer (kind = int), parameter :: N = 500
+    integer (kind = int), parameter :: N = 500, k = 4
     real (kind = double), parameter :: L = 10, rc = 5
+    real (kind = double), parameter :: l_box = 2.5d00
+    real (kind = double), parameter :: E = -680.d00
 
 end module fcc_parameters
