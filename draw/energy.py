66d3c26ff1c1567ad45881314de410a1a0dc8277
Braisa <oterolemabrais@gmail.com>
2025-11-06 15:25:34 +0100

    Maxwell progress

--- draw/energy.py
+++ draw/energy.py
@@ -17,9 +17,9 @@ fig, ax = plt.subplots()
 steps = logging_interval * len(energy)
 steps_lin = logging_interval * (1 + np.arange(len(energy)))
 
-ax.plot(steps_lin, energy, ls = "solid", color = "tab:blue")
-ax.plot(steps_lin, np.mean(energy) * np.ones_like(steps_lin), ls = "dashed", color = "tab:orange", label="All steps mean")
-ax.plot(steps_lin, np.mean(energy[20:]) * np.ones_like(steps_lin), ls = "dashed", color = "tab:purple", label="Last steps mean")
+ax.plot(steps_lin, energy, ls = "solid", color = "tab:orange")
+#ax.plot(steps_lin, np.mean(energy) * np.ones_like(steps_lin), ls = "dashed", color = "tab:orange", label="All steps mean")
+#ax.plot(steps_lin, np.mean(energy[20:]) * np.ones_like(steps_lin), ls = "dashed", color = "tab:purple", label="Last steps mean")
 
 ax.set_xlim(left=np.min(steps_lin), right=np.max(steps_lin))
 
--- draw/maxwell.py
+++ draw/maxwell.py
@@ -8,3 +8,25 @@ path = os.path.dirname(__file__) + "/"
 rva = pd.read_csv(path + "big_rva.txt", sep=" ")
 
 rva["v2"] = rva["vx"]**2 + rva["vy"]**2 + rva["vz"]**2
+
+names = ("vx", "vy", "vz", "v2")
+xlabels = (r"$x$", r"$y$", r"$z$", r"$r^2$")
+ylabels = (r"$v_x$", r"$v_y$", r"$v_z$", r"$v^2$")
+colors = ("red", "green", "blue", "orange")
+
+for (name, xlabel, ylabel, color) in zip(names, xlabels, ylabels, colors):
+    fig, ax = plt.subplots()
+
+    plt.hist(rva[name], bins=50, histtype="step", color=f"tab:{color}")
+    
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    if not name == "v2":
+        ax.set_xlim(left=-4, right=+4)
+    else:
+        ax.set_xlim(left=0, right=25)
+    ax.set_ylim(bottom=0)
+
+    fig.tight_layout()
+    fig.savefig(path + f"maxwell_{name}.pdf", dpi=300, bbox_inches="tight")
